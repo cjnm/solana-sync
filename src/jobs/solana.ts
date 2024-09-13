bc2ebbddb42b965f5234c4f7e24f61f5907dcc01
cjnm <seejan.raj@live.com>
2024-09-13 22:04:46 +0545

    updated

--- src/jobs/solana.ts
+++ src/jobs/solana.ts
@@ -1,132 +1,180 @@
 import { SolanaWalletTokenBalanceModel } from "../models/SolanaWalletTokenBalance";
 import {
-	getTokenBalances,
-	getAccountTransactions,
-	getTransactionDetails,
+  getTokenBalances,
+  getAccountTransactions,
+  getTransactionDetails,
 } from "../controllers/Solana";
 import { SolanaAccountTransactionsModel } from "../models/SolanaAccountTransactions";
 import { SolanaTransactionDetailsModel } from "../models/SolanaTransactionDetails";
 import { AccountTotalBalance } from "../models/AccountTotalBalance";
+import { Account } from "../models/Account";
+import { TransactionHistory } from "../models/TransactionHistory";
 
 type transaction = {
-	blockTime: number;
-	confirmationStatus: string;
-	err: null | string;
-	memo: null | string;
-	signature: string;
-	slot: number;
+  blockTime: number;
+  confirmationStatus: string;
+  err: null | string;
+  memo: null | string;
+  signature: string;
+  slot: number;
 };
 
 // save soalan token balances
 const saveTokenBalances = async (solanaWalletAddress: string, user: string) => {
-	const tokenBalance = await getTokenBalances(solanaWalletAddress).catch(
-		(error) => {
-			throw new Error(error);
-		},
-	);
+  const tokenBalance = await getTokenBalances(solanaWalletAddress).catch(
+    (error) => {
+      throw new Error(error);
+    }
+  );
 
-	if (tokenBalance.error) {
-		throw new Error(tokenBalance.error_message);
-	}
+  if (tokenBalance.error) {
+    throw new Error(tokenBalance.error_message);
+  }
 
-	if (!tokenBalance?.data) {
-		throw new Error("No data.");
-	}
+  if (!tokenBalance?.data) {
+    throw new Error("No data.");
+  }
 
-	const { address } = tokenBalance.data;
-	await SolanaWalletTokenBalanceModel.findOneAndUpdate(
-		{ address },
-		{ ...tokenBalance.data, user, account: address },
-		{
-			upsert: true,
-		},
-	);
-	console.log("token balance saved");
+  const { address } = tokenBalance.data;
+  await SolanaWalletTokenBalanceModel.findOneAndUpdate(
+    { address },
+    { ...tokenBalance.data, user, account: address },
+    {
+      upsert: true,
+    }
+  );
+  console.log("token balance saved");
 
-	// saving aggregate balance
-	const total_balance = tokenBalance.data.items.reduce((total, datum) => {
-		return total + datum.quote;
-	}, 0);
+  // saving aggregate balance
+  const total_balance = tokenBalance.data.items.reduce((total, datum) => {
+    return total + datum.quote;
+  }, 0);
 
-	await AccountTotalBalance.findOneAndUpdate(
-		{ account: address },
-		{ amount: total_balance, user, account: address, sync_time: 1 },
-		{
-			upsert: true,
-		},
-	);
+  await AccountTotalBalance.findOneAndUpdate(
+    { account: address },
+    { amount: total_balance, user, account: address, sync_time: 1 },
+    {
+      upsert: true,
+    }
+  );
 };
 
 const saveWalletTransactions = async (
-	solanaWalletAddress: string,
-	user: string,
+  solanaWalletAddress: string,
+  user: string
 ) => {
-	const transactions = await getAccountTransactions(solanaWalletAddress).catch(
-		(error) => {
-			throw new Error(error);
-		},
-	);
+  const accounts = await Account.findOne({
+    wallet_address: solanaWalletAddress,
+  }).catch((error) => {
+    throw new Error(error);
+  });
 
-	if (transactions.status !== "success") throw new Error(transactions.message);
+  const transactions = await getAccountTransactions(solanaWalletAddress).catch(
+    (error) => {
+      throw new Error(error);
+    }
+  );
 
-	// Prepare bulk operations
-	const bulkOps = transactions.result.data.map((transaction: any) => ({
-		updateOne: {
-			filter: { signature: transaction.signature },
-			update: { $set: { ...transaction, user, account: solanaWalletAddress } },
-			upsert: true,
-		},
-	}));
+  if (transactions.status !== "success") throw new Error(transactions.message);
 
-	SolanaAccountTransactionsModel.bulkWrite(bulkOps)
-		.then((result) => {
-			console.log("Bulk upsert completed:", result);
-		})
-		.catch((error) => {
-			console.error("Error in bulk upsert:", error);
-			throw new Error(error);
-		});
+  // Prepare bulk operations
+  const bulkOps = transactions.result.data.map((transaction: any) => ({
+    updateOne: {
+      filter: { signature: transaction.signature },
+      update: { $set: { ...transaction, user, account: solanaWalletAddress } },
+      upsert: true,
+    },
+  }));
 
-	const tx_hashes = transactions.result.data.map(
-		(item: transaction) => item.signature,
-	);
+  SolanaAccountTransactionsModel.bulkWrite(bulkOps)
+    .then((result) => {
+      console.log("Bulk upsert completed:", result);
+    })
+    .catch((error) => {
+      console.error("Error in bulk upsert:", error);
+      throw new Error(error);
+    });
 
-	while (tx_hashes.length > 0) {
-		const chunk = tx_hashes.slice(0, 50); // Get the first 50 elements
-		tx_hashes.splice(0, 50); // Remove the first 50 elements
+  const tx_hashes = transactions.result.data.map(
+    (item: transaction) => item.signature
+  );
 
-		const transactionDetails = await getTransactionDetails(chunk).catch(
-			(error) => {
-				throw new Error(error);
-			},
-		);
+  while (tx_hashes.length > 0) {
+    const chunk = tx_hashes.slice(0, 50); // Get the first 50 elements
+    tx_hashes.splice(0, 50); // Remove the first 50 elements
 
-		if (transactionDetails.status !== "success")
-			throw new Error(transactionDetails.message);
+    const transactionDetails = await getTransactionDetails(chunk).catch(
+      (error) => {
+        throw new Error(error);
+      }
+    );
 
-		// Prepare bulk operations
-		const _bulkOps = transactionDetails.result.map((transaction: any) => ({
-			updateOne: {
-				filter: { transactionHash: transaction.transactionHash },
-				update: {
-					$set: { ...transaction, user, account: solanaWalletAddress },
-				},
-				upsert: true,
-			},
-		}));
+    if (transactionDetails.status !== "success")
+      throw new Error(transactionDetails.message);
 
-		// Execute bulkWrite
-		SolanaTransactionDetailsModel.bulkWrite(_bulkOps)
-			.then((result: any) => {
-				console.log("Bulk upsert completed:", result);
-			})
-			.catch((error: any) => {
-				console.error("Error in bulk upsert:", error);
-				throw new Error(error);
-			});
-	}
+    // Prepare bulk operations
+    const _bulkOps = transactionDetails.result.map((transaction: any) => ({
+      updateOne: {
+        filter: { transactionHash: transaction.transactionHash },
+        update: {
+          $set: { ...transaction, user, account: solanaWalletAddress },
+        },
+        upsert: true,
+      },
+    }));
 
-	console.log("Transaction sync done");
+    // Execute bulkWrite
+    SolanaTransactionDetailsModel.bulkWrite(_bulkOps)
+      .then((result: any) => {
+        console.log("Bulk upsert completed:", result);
+      })
+      .catch((error: any) => {
+        console.error("Error in bulk upsert:", error);
+        throw new Error(error);
+      });
+
+    // prepare transaction history bulk write - backward compactibility
+
+    // Prepare bulk operations
+    const __bulkOps = transactionDetails.result.map((transaction: any) => ({
+      updateOne: {
+        filter: { tx_hash: transaction.transactionHash },
+        update: {
+          $set: {
+            ...transaction,
+            user,
+            account: solanaWalletAddress,
+            chain: "solana",
+            chain_name: "Solana",
+            chain_logo:
+              "https://www.datocms-assets.com/86369/1670614895-solana-icon-white.svg",
+            standard: "spl",
+            time_at: transaction?.data[0]?.timestamp || "",
+            tx_hash: transaction.transactionHash,
+            account_id: accounts._id,
+            account_name: accounts.name,
+            account_color: accounts.account_color,
+            quantity: 0,
+            tx: {
+              name: "",
+            },
+          },
+        },
+        upsert: true,
+      },
+    }));
+
+    // Execute bulkWrite
+    TransactionHistory.bulkWrite(__bulkOps)
+      .then((result: any) => {
+        console.log("Bulk upsert completed:", result);
+      })
+      .catch((error: any) => {
+        console.error("Error in bulk upsert:", error);
+        throw new Error(error);
+      });
+  }
+  console.log("Transaction sync done");
 };
 
 export { saveTokenBalances, saveWalletTransactions };
---
+++ src/models/TransactionHistory.ts
@@ -0,0 +1,48 @@
+import { Schema, model, type InferSchemaType } from "mongoose";
+
+const transactionHistorySchema = new Schema(
+  {
+    account: { index: true, type: String },
+    standard: { type: String, default: "erc20" },
+    cate_id: Schema.Types.String,
+    chain: Schema.Types.String,
+    chain_logo: Schema.Types.String,
+    tx_hash: String,
+    project_id: String,
+    receives: [
+      {
+        amount: Number,
+        from_addr: String,
+        token_id: String,
+      },
+    ],
+    sends: [
+      {
+        amount: Number,
+        to_addr: String,
+        token_id: String,
+      },
+    ],
+    token_approve: [
+      {
+        spender: String,
+        token_id: String,
+        value: Number,
+      },
+    ],
+    time_at: { type: Number, index: true },
+    tx: {},
+    other_addr: String,
+    rest: {},
+  },
+  { timestamps: true }
+);
+
+export type SchemaType = InferSchemaType<typeof transactionHistorySchema>;
+
+const TransactionHistory = model<SchemaType>(
+  "TransactionHistory",
+  transactionHistorySchema
+);
+
+export { TransactionHistory };
